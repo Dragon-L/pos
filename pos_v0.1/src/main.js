8843362b19ea3dd2f795ae1133e63742841ffc89
Dragon-L <530593201@qq.com>
2016-05-24 17:37:26 +0800

    update

--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -1,29 +1,51 @@
 function printInventory(inputs){
-    var goodsArray = new Array(),total = 0;
-    for(x in inputs){
-        insertGoods(inputs[x],goodsArray);
-    }
-    var out = "***<没钱赚商店>购物清单***\n";
-    for(x in goodsArray){
-        var totalPrice = goodsArray[x].number*goodsArray[x].price;
-        out += "名称：" + goodsArray[x].name +"，数量："+goodsArray[x].number+
-            goodsArray[x].unit+"，单价："+goodsArray[x].price.toFixed(2)+"(元)，小计："+
-            totalPrice.toFixed(2)+"(元)\n";
-        total = total + totalPrice;
-    }
-    out += '----------------------\n'+'总计：'+total.toFixed(2)+'(元)\n'+'**********************';
-    console.log(out);
+    var cartItems = [];
+    var total = 0;
+    cartItems = jasonToCartItems(inputs);
+    total = calculateCartItems(cartItems);
+    printCartItems(cartItems,total);
 }
-function insertGoods(input,array){
-   if (array[input.barcode]){
-        array[input.barcode].number++;
+function jasonToCartItems(jasons) {
+    var cartItems = [];
+    for(var i = 0,len = jasons.length;i < len;i++){
+        pushCartItem(jasons[i],cartItems);
     }
-    else {
-        array[input.barcode] = new Object();
-        array[input.barcode].name = input.name;
-        array[input.barcode].number = 1;
-        array[input.barcode].unit = input.unit;
-        array[input.barcode].price = input.price;
-        array[input.barcode].barcode = input.barcode;
+    return cartItems;
+}
+function pushCartItem(jason,array){
+    if (array[jason.barcode]){
+        array[jason.barcode].count++;
+    } else {
+        array[jason.barcode] = {};
+        array[jason.barcode].name = jason.name;
+        array[jason.barcode].count = 1;
+        array[jason.barcode].unit = jason.unit;
+        array[jason.barcode].price = jason.price;
+        array[jason.barcode].barcode = jason.barcode;
     }
+}
+function calculateCartItems(cartItems) {
+    for(i in cartItems){
+        cartItems[i].subtotal = cartItems[i].count * cartItems[i].price;
+    }
+    var total = 0.00;
+    for(i in cartItems){
+        total += cartItems[i].subtotal;
+    }
+    return total;
+}
+function printCartItems(cartItems,total) {
+    var out = "***<没钱赚商店>购物清单***\n";
+    for(i in cartItems){
+        out += "名称：" + cartItems[i].name +
+            "，数量："+ cartItems[i].count + cartItems[i].unit +
+            "，单价：" + cartItems[i].price.toFixed(2) +
+            "(元)，小计：" + cartItems[i].subtotal.toFixed(2) +
+            "(元)\n";
+    }
+
+    out += '----------------------\n' +
+        '总计：'+total.toFixed(2) + '(元)\n' +
+        '**********************';
+    console.log(out);
 }
